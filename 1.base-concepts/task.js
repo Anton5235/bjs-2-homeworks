74f883fe295f0e4ddfc207b917d9c7d089574e36
Антон <fisher5235@yandex.ru>
2023-02-21 00:51:03 +0300

    Commit task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,11 +19,17 @@ console.log(solveEquation(1,2,3))
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   if(isNaN(percent)) {
     console.log("Первый параметр не является числом!")
-  };
+  } ;
   if(isNaN(contribution)) {
     console.log("Второй параметр не является числом!")
   };
   if(isNaN(amount)) {
     console.log("Третий параметр не является числом!")
   };
+  const s = amount - contribution;
+  const p = (percent / 100) / 12;
+  const monthPay = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+  const finalPay = (monthPay * countMonths).toFixed(2);
+return finalPay
 }
+console.log(calculateTotalMortgage(15, 0, 10000, 36));
