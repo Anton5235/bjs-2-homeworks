9b9d5624cf647e4b009a0b6baade2e4f8725ef82
Антон Рыбкин <fisher5232@yandex.ru>
2023-02-20 15:51:25 +0300

    "Commit1"

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  let root;
+  if(discriminant === 0) {
+    root = -b / (2 * a);
+    arr.push(root)
+  } else if(discriminant > 0) {
+    root = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+    arr.push(root, root2)
+  }
   return arr;
 }
+console.log(solveEquation(1,2,3))
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
