c66af38adad59a54cc94704c7bebbda539205880
Антон <fisher5235@yandex.ru>
2023-02-25 01:32:37 +0300

    Task 1, 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,23 +1,40 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  let avg = arr.reduce((total, amount) => (total + amount / arr.length), 0);
+  return { min: Math.min(...arr), max: Math.max(...arr), avg: Number((avg).toFixed(2)) };
 }
+console.log(getArrayParams(1, 2, 3, -100, 10))
 
 function summElementsWorker(...arr) {
-
-}
+  return arr.reduce((total, amount) => (total + amount), 0)
+};
 
 function differenceMaxMinWorker(...arr) {
-
+  let difference = 0;
+  if(arr.length > 0) {
+    difference += Math.max(...arr) - Math.min(...arr) };
+  return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
-
-}
+  const sumEvenElement = arr.reduce((total, amount) => {
+    if(amount % 2 === 0) {
+      total = total + amount;
+    }
+    return total;
+  }, 0);
+  const sumOddElement = arr.reduce((total, amount) => {
+    if(amount % 2 !== 0) {
+      total = total + amount;
+    }
+    return total;
+  }, 0);
+  return sumEvenElement - sumOddElement;
+};
 
 function averageEvenElementsWorker(...arr) {
-
-}
+  const newArr = arr.filter(elem => elem % 2 === 0);
+  return newArr.reduce((total, amount) => (total + amount / newArr.length), 0);
+};
 
 function makeWork (arrOfArr, func) {
 
