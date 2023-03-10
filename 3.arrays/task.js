1ea5828b2e3ae56f9c7945fcd191d2f7f48f30e3
Антон <fisher5235@yandex.ru>
2023-03-11 01:00:40 +0300

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
-function compareArrays(arr1, arr2) {
-  
-}
+function compareArrays (arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((array, index) => array === arr2[index])}
+  console.log(compareArrays([8, 1, 2], [8, 1]))
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter(users => users.gender === gender).map(gender => gender.age);
+    return result.reduce(((total, amount) => total + amount / result.length),0)}
+  const people = [
+    { firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской" },
+    { firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской" },
+    { firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский" },
+    { firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский" },
+    { firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский" },
+    { firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский" },
+    { firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской" },
+    { firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской" },
+    { firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской" },
+    { firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский" },
+    { firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской" },
+    { firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской" },
+    { firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской" },
+    { firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской" },
+  ]
+  console.log(getUsersNamesInAgeRange(people, "мужской"));
