2a18a5ce56bf645050a847dbb7785be1f4163bd2
Антон <fisher5235@yandex.ru>
2023-03-13 20:53:06 +0300

    123

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,7 +1,7 @@
 ﻿function parseCount (numb) {
     let newNumb = Number.parseFloat(numb)
-    if(newNumb === NaN) {
-      console.log("Невалидное значение")
+    if(isNaN(newNumb)) {
+      return "Невалидное значение"
     }
     return newNumb
   }
