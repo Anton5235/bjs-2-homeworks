318e90a4daa5ab31b5a53b9b8457012a3b267dda
Антон <fisher5235@yandex.ru>
2023-05-01 00:51:47 +0300

    Task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,18 +5,26 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 Student();
-let student1 = new Student();
-let student2 = new Student();
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
 let student3 = new Student();
 
 
 Student.prototype.setSubject = function (subjectName) {
-  Student.setSubject(subjectName)
+  Student.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marksToAdd) {
-  Student.hasOwnProperty(this.marks);
-  Student.addMarks(...marksToAdd)
+  Student.addMarks = [...marksToAdd];
 }
 
 Student.prototype.getAverage = function () {
@@ -30,3 +38,4 @@ Student.prototype.getAverage = function () {
 Student.prototype.exclude = function (reason) {
   
 }
+
