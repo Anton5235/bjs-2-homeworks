a93055b166f12dcdb2f8e1fa596b681f3bda7265
Антон <fisher5235@yandex.ru>
2023-05-17 23:50:44 +0300

    Error

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,3 +14,39 @@ function parseCount (numb) {
     } 
 }
   
+class Triangle {
+  constructor(a, b, c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+   
+    if(a, b, c <= 0 || a + b < c || a + c < b || b + c < a ){
+      throw new Error ("Треугольник с такими сторонами не существует")
+    }
+
+  }
+
+  
+  get perimeter () {
+      return this.a + this.b + this.c
+  }
+
+  get area () {
+    let s = 0.5 * (this.a + this.b + this.c)
+    return (Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c))).toFixed(3)
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c)
+  } catch (error) {
+     return obj = {
+      get perimeter () {
+      return 'Ошибка! Треугольник не существует'},
+      get area () {
+        return 'Ошибка! Треугольник не существует'}
+      }
+    }
+  }
+  
