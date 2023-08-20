37759c7ca567fc99e3351ee594062074bee89074
Антон <fisher5235@yandex.ru>
2023-08-21 02:10:56 +0300

    1

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,3 +1,30 @@
 class AlarmClock {
-    constructor(alarmCollection)
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if(!time || !callback) {
+            throw new Error ('Отсутствуют обязательные аргументы')
+        }
+
+        if(this.alarmCollection.includes(time)) {
+            console.warn('Уже присутствует звонок на это же время')
+        }
+
+        let timeArray = {
+            callback: callback,
+            time: time,
+            canCall: true
+        }
+
+        this.alarmCollection.push(timeArray)
+    }
+
+    removeClock() {
+        this.alarmCollection = this.alarmCollection.filter((item) => item.time !== time);
+    }
+    
+
 }
