9366fdae029c23fa3aca93bfd435f1be53f568e3
Антон <fisher5235@yandex.ru>
2023-08-24 01:52:45 +0300

    1

--- 7.async/task.js
+++ 7.async/task.js
@@ -4,27 +4,62 @@ class AlarmClock {
         this.intervalId = null;
     }
 
-    addClock(time, callback) {
-        if(!time || !callback) {
+    addClock(currentTime, callback) {
+        if(!currentTime || !callback) {
             throw new Error ('Отсутствуют обязательные аргументы')
         }
 
-        if(this.alarmCollection.includes(time)) {
+        if(this.alarmCollection.includes(currentTime)) {
             console.warn('Уже присутствует звонок на это же время')
         }
 
         let timeArray = {
             callback: callback,
-            time: time,
+            time: currentTime,
             canCall: true
         }
 
         this.alarmCollection.push(timeArray)
     }
 
-    removeClock() {
+    removeClock(time) {
         this.alarmCollection = this.alarmCollection.filter((item) => item.time !== time);
     }
     
+    getCurrentFormattedTime(){
+        let date = new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+        return date;
+    }
 
+    start() {
+        if (this.intervalId) {
+            return
+        }
+
+        this.intervalId = setInterval(() => 
+            this.alarmCollection.forEach((item) => {
+                if (this.getCurrentFormattedTime() == item.time){
+                    item.canCall == false;  
+                    item.callback();
+                    item.canCall = !item.canCall;    
+                }
+            }), 1000);
+    }
+
+    stop(){
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls(){
+        this.alarmCollection.forEach((item) => item.canCall = true);
+    }
+
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = []
+    }
 }
