faaf818a7c4ada525d9665c1b1def911d3a94872
Антон <fisher5235@yandex.ru>
2023-08-28 00:22:47 +0300

    1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,30 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash == hash);
+        if(objectInCache) {
+            console.log('Из кэша: ' + objectInCache.value);
+            return 'Из кэша: ' + objectInCache.value;
+        }
+
+        let result = func(...args);
+        let calculate = {
+            hash: hash,
+            value: result
+        }
+        cache.push(calculate);
+        if(cache.length > 5) {
+            cache.shift()
+        }
+        console.log('Вычисляем: ' + result);
+        return 'Вычисляем: ' + result
+    }
+    return wrapper
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
